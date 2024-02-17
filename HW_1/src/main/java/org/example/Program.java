f1fedb27c3d9d4e5716bd454ad00ab775d7e5683
Denis <damned_wolf@mail.ru>
2024-02-17 22:30:24 +0300

    HW1

---
+++ HW_1/src/main/java/org/example/Program.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.example.calc.Calculator;
+import org.example.utils.Decorator;
+
+/*
+
+javac -sourcepath ./java -d out java/ru/geekbrains/core/lesson1/task1/Program.java
+java -classpath ./out ru.geekbrains.core.lesson1.task1.Program
+javadoc -encoding utf8 -d docs -sourcepath ./java -cp ./out -subpackages ru
+
+https://www.docker.com/products/docker-desktop/
+https://hub.docker.com/
+ */
+
+/**
+ * Основной класс приложения. Здесь мы можем описать
+ * его основное назначение и способы взаимодействия с ним.
+ */
+public class Program {
+
+
+    public static void main(String[] args) {
+
+        int result = Calculator.add(2, 2);
+        System.out.println(Decorator.decorate(result));
+        result = Calculator.sub(2, 2);
+        System.out.println(Decorator.decorate(result));
+        result = Calculator.mul(2, 2);
+        System.out.println(Decorator.decorate(result));
+        result = Calculator.div(2, 2);
+        System.out.println(Decorator.decorate(result));
+    }
+
+}
+
